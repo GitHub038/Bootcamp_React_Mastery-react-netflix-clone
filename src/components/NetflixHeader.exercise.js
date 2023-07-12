f0b12c11afb6a30e661a15d8572695abb37348c5
SALIM OTMANI <salim.otmani.pro@gmail.com>
2023-07-12 16:13:54 +0200

    add exo 1

--- src/components/NetflixHeader.exercise.js
+++ src/components/NetflixHeader.exercise.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {imagePathOriginal, TYPE_MOVIE} from '../config'
 import {HeaderSkeleton} from './skeletons/HeaderSkeleton'
 
 // 🐶 importe le hook 'useFetchData' depuis 'utils/hooks'
 // 🐶 importe le 'clientNetFlix' depuis 'utils/clientApi'
 // il nous permettra de faire les appels API REST avec 'axios'
-// 🐶 importe 'authNetflix' il nous sera utile pour récuperer 
+// 🐶 importe 'authNetflix' il nous sera utile pour récuperer
 // le 'token' de l'utilisateur connecté
 // 🤖 import * as authNetflix from '../utils/authNetflixProvider'
+
+import {useFetchData} from 'utils/hooks'
+import {clientNetFlix} from 'utils/clientApi'
+import * as authNetflix from '../utils/authNetflixProvider'
 
 // 👨‍✈️ Trois choses à gérer dans ce composant :
 //
@@ -20,6 +24,8 @@ import {HeaderSkeleton} from './skeletons/HeaderSkeleton'
 //  - faire un appel API DELETE /bookmark/tv ou /bookmark/movie
 const NetflixHeader = ({movie, type = TYPE_MOVIE}) => {
   // 🐶 utilise le hook 'useFetchData' pour avoir : data, execute
+  const {data, execute} = useFetchData()
+
   const title = type === TYPE_MOVIE ? movie?.title : movie?.name
   const imageUrl = `${imagePathOriginal}${movie?.backdrop_path}`
   const banner = {
@@ -31,28 +37,59 @@ const NetflixHeader = ({movie, type = TYPE_MOVIE}) => {
     height: '448px',
   }
 
-   // 🐶 utilise le hook 'useEffect' pour faire l'appel API GET '/bookmark'
-   // pour cela utilise :
-   // 🤖
-   // authNetflix.getToken()
-   // execute(clientNetFlix(`bookmark`, {token}))
-   // NOTE : authNetflix.getToken() s'utilise de manière asynchrone avec 'await'
+  // 🐶 utilise le hook 'useEffect' pour faire l'appel API GET '/bookmark'
+  // pour cela utilise :
+  // 🤖
+  // authNetflix.getToken()
+  // execute(clientNetFlix(`bookmark`, {token}))
+  // NOTE : authNetflix.getToken() s'utilise de manière asynchrone avec 'await'
 
-   // 🐶 créé un boolean 'isInList' permetant de s'avoir si 'movie.id' est deja 
-   // dans la liste des favoris récuperer par l'api '/bookmark'
-   // rapel du format des données reçues
-   // data.bookmark.movies[ids de films]
-   // data.bookmark.tv[ids de séries]
+  useEffect(() => {
+    async function getTokenExecute() {
+      const token = await authNetflix.getToken()
+      execute(clientNetFlix(`bookmark`, {token}))
+    }
+    getTokenExecute()
+  }, [execute])
 
-   // 🐶 créé une fonction async 'handleAddToListClick' qui fera l'appel API REST
-   // '/bookmark/tv' ou '/bookmark/movie'
-   // utilise 'clientNetFlix' car il permet de passer des options : {token,data,method}
-   // - passe le 'token'
-   // - passe comme 'data' : id (l'id du film/serie)
-   // - passe come 'method' 'POST'
+  // 🐶 créé un boolean 'isInList' permetant de s'avoir si 'movie.id' est deja
+  // dans la liste des favoris récuperer par l'api '/bookmark'
+  // rapel du format des données reçues
+  // data.bookmark.movies[ids de films]
+  // data.bookmark.tv[ids de séries]
+  const isInList = data?.bookmark[
+    type === TYPE_MOVIE ? 'movies' : 'series'
+  ]?.includes(movie?.id)
 
-   // 🐶 créé une fonction async 'handleDeleteToListClick'
-   // pareil que précedement mais en utilisant la methode 'DELETE'
+  // 🐶 créé une fonction async 'handleAddToListClick' qui fera l'appel API REST
+  // '/bookmark/tv' ou '/bookmark/movie'
+  // utilise 'clientNetFlix' car il permet de passer des options : {token,data,method}
+  // - passe le 'token'
+  // - passe comme 'data' : id (l'id du film/serie)
+  // - passe come 'method' 'POST'
+  const handleAddToListClick = async () => {
+    const token = await authNetflix.getToken()
+    execute(
+      clientNetFlix(`bookmark/${type}`, {
+        token,
+        data: {id: movie.id},
+        method: 'POST',
+      }),
+    )
+  }
+
+  // 🐶 créé une fonction async 'handleDeleteToListClick'
+  // pareil que précedement mais en utilisant la methode 'DELETE'
+  const handleDeleteToListClick = async () => {
+    const token = await authNetflix.getToken()
+    execute(
+      clientNetFlix(`bookmark/${type}`, {
+        token,
+        data: {id: movie.id},
+        method: 'DELETE',
+      }),
+    )
+  }
 
   if (!movie) {
     return <HeaderSkeleton></HeaderSkeleton>
@@ -67,9 +104,21 @@ const NetflixHeader = ({movie, type = TYPE_MOVIE}) => {
             1. Premier bouton : 'Supprimer de ma liste' avec onClick={handleDeleteToListClick}
             2. Deuximème bouton : 'Ajouter à ma liste' onClick={handleAddToListClick}
            */}
-          <button className="banner__button banner__buttonInfo">
-            Ajouter à ma liste
-          </button>
+          {isInList ? (
+            <button
+              className="banner__button banner__buttonInfo"
+              onClick={handleDeleteToListClick}
+            >
+              Supprimer de ma liste
+            </button>
+          ) : (
+            <button
+              className="banner__button banner__buttonInfo"
+              onClick={handleAddToListClick}
+            >
+              Ajouter à ma liste
+            </button>
+          )}
         </div>
         <h1 className="synopsis">{movie?.overview ?? '...'}</h1>
       </div>
