579272b3bb129e53618883c85b137e311c9da5e6
sao <c_pasmoi@outlook.com>
2023-10-06 16:54:14 +0200

    Bonus 1 done

--- src/__tests__/App.exercise.js
+++ src/__tests__/App.exercise.js
@@ -2,11 +2,41 @@
 // import * as React from 'react'
 // 🐶 importe 'render' 'screen' 'waitForElementToBeRemoved' depuis 'test/test-utils'
 // 🐶 importe également
-import {render, screen, waitForElementToBeRemoved} from 'test/test-utils'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+  sampleMovie,
+  bookmark,
+  resultsMovies,
+} from 'test/test-utils'
 // import userEvent from '@testing-library/user-event'
 // import {App} from 'App'
 import userEvent from '@testing-library/user-event'
 import {App} from 'App'
+import {AUTH_URL, API_URL, localStorageTokenKey} from 'config'
+import {server, rest} from 'mocks'
+
+beforeEach(() => {
+  const user = {id: '1', username: 'fakeUsername', token: 'FAKE_TOKEN'}
+  server.use(
+    rest.get(`${AUTH_URL}/me`, async (req, res, ctx) => {
+      return res(ctx.json({user}))
+    }),
+    rest.get(`${API_URL}/movie/:id`, async (req, res, ctx) => {
+      return res(ctx.json({sampleMovie}))
+    }),
+    rest.get(`${API_URL}/tv/:id`, async (req, res, ctx) => {
+      return res(ctx.json({sampleMovie}))
+    }),
+    rest.get(`${AUTH_URL}/bookmark`, async (req, res, ctx) => {
+      return res(ctx.json({bookmark}))
+    }),
+    rest.get(`${API_URL}/*`, async (req, res, ctx) => {
+      return res(ctx.json({resultsMovies}))
+    }),
+  )
+})
 
 // 🐶 fait un test en async
 test("rendu de l'app avec page de Login", async () => {
@@ -26,7 +56,42 @@ test("rendu de l'app avec page de Login", async () => {
 })
 
 //bonus-1
-test.todo("rendu de l'app en mode connecté")
+test("rendu de l'app en mode connecté", async () => {
+  const connexion = 'Connexion'
+  const register = 'Inscrivez vous'
+  const user = {id: '1', username: 'fakeUsername', token: 'FAKE_TOKEN'}
+  window.localStorage.setItem(localStorageTokenKey, user.token)
+  render(<App />)
+  await waitForElementToBeRemoved(() => screen.getByRole('alert'))
+  expect(
+    screen.queryByText('heading', {name: connexion}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.queryByText('heading', {name: register}),
+  ).not.toBeInTheDocument()
+  expect(screen.getByRole('heading', {name: 'Accueil'})).toBeInTheDocument()
+  expect(screen.getByRole('heading', {name: 'Series'})).toBeInTheDocument()
+  expect(screen.getByRole('heading', {name: 'Films'})).toBeInTheDocument()
+  expect(
+    screen.getByRole('heading', {name: 'Nouveautés les plus regardées'}),
+  ).toBeInTheDocument()
+  expect(screen.getByRole('heading', {name: 'Ma liste'})).toBeInTheDocument()
+  expect(
+    screen.getByRole('heading', {name: 'Films Netflix'}),
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole('heading', {name: 'Séries Netflix'}),
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole('heading', {name: 'Les mieux notés'}),
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole('heading', {name: 'Action & aventure'}),
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole('heading', {name: 'Les meilleurs Thriller'}),
+  ).toBeInTheDocument()
+})
 
 //bonus-2
 test.todo('rendu de route la /series')
